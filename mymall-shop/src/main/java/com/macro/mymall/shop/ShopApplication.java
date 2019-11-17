10bbac539f461a4727766b129f5504d99cb34abd
Arsense <466217395@qq.com>
2019-11-17 19:29:59 +0800

    shop端初始化

--- mymall-shop/src/main/java/com/macro/mymall/shop/ShopApplication.java
+++ mymall-shop/src/main/java/com/macro/mymall/shop/ShopApplication.java
@@ -1,4 +1,4 @@
-package com.macro.mymall.admin;
+package com.macro.mymall.shop;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -6,17 +6,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+/**
+ *
+ */
 @SpringBootApplication
-public class AdminApplication {
+public class ShopApplication {
 
-	private static final Logger LOG = LoggerFactory.getLogger(AdminApplication.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ShopApplication.class);
+
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(AdminApplication.class, args);
 
-		String serverPort = context.getEnvironment().getProperty("server.port");
+		ApplicationContext context = SpringApplication.run(ShopApplication.class, args);
 		//输入访问链接
-
+		String serverPort = context.getEnvironment().getProperty("server.port");
 		LOG.info("MyMall started at http://localhost:" + serverPort);
 		LOG.info("api文档列表  http://localhost:" + serverPort + "/swagger-ui.html");
 
---
+++ mymall-shop/src/test/com/macro/mymall/shop/ShopApplicationTests.java
@@ -0,0 +1,10 @@
+package com.macro.mymall.shop;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+
+
+}
